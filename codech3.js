fce0d601a28cc4b9241521da2dafda8c38f178d9
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-28 23:11:53 +0800

    Added Coding Challenge #3

---
+++ codech3.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// Rewrite the calcAverageHumanAge function but this time as an arrow function and using chaining
+
+const calcAverageHumanAge = function (ages) {
+  //Map - Multiply all elements of array according to condition
+  //Filter - Remove elements according to condition
+  //Reduce - Combine all elements into one variable
+  // acc + age / arr.length = acc / arr.length + age / arr.length
+
+  const humanAges = ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+  return humanAges;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
+
+// Jonas Schmedtmann Implementation
+/*
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
+
+*/
